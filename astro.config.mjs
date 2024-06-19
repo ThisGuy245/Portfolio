6062f553cadc432f604c89b5b214c9baeddd2973
ThisGuy245 <Thomas@je3.com>
2024-06-19 14:03:21 +0100

    Website works again and i've almost cracked localisation!!

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import tailwind from '@astrojs/tailwind';
+import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [mdx(), tailwind(), svelte()]
+});
