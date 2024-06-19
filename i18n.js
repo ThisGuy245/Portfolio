c086fa2b0979fbd8fc27304c50e0bb26c408a608
ThisGuy245 <Thomas@je3.com>
2024-06-19 13:43:39 +0100

    Second Dev commit, begun working on localisation

--- i18n.js
+++ i18n.js
@@ -1,8 +1,10 @@
-// src/i18n.js
+// i18n.js
 import i18n from 'i18next';
 import HttpBackend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
+
+const supportedLanguages = ['en', 'fr'];
 
 i18n
     .use(HttpBackend)
@@ -12,8 +14,12 @@ i18n
         fallbackLng: 'en',
         debug: true,
         interpolation: {
-            escapeValue: false, // React already does escaping
+            escapeValue: false,
+        },
+        backend: {
+            loadPath: '/locales/{{lng}}/{{ns}}.json' //
         }
     });
 
+export { supportedLanguages };
 export default i18n;
